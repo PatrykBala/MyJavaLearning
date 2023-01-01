eac2447e9d71282522d92a48f6169dc7528d5483
Patryk Bala <patrykbala89@gmail.com>
2023-01-01 11:09:38 +0100

    first variable use

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My first program");
+        int age = 12;
+        var age2 = 11;
+        var word = " eh my mind";
+        char one = '\\';
+        String word2 = " I like java";
+        System.out.println("My first Java program " + age + word + word2 + one);
     }
 }
