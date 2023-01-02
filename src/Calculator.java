4349ac41df8cdb7fd5027e950953e9208b1a9568
Patryk Bala <patrykbala89@gmail.com>
2023-01-02 21:58:38 +0100

    two variables a + b =

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static final int A = 10;
+    public static void main(String[] args) {
+        int b;
+        b = 9;
+        System.out.println("A + b = " + (A + b));
+    }
+}
