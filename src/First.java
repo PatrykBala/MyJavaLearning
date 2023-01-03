3cdfe27c4d69e6cf32e03a200df3373311dabab3
Patryk Bala <patrykbala89@gmail.com>
2023-01-03 21:55:29 +0100

    First variable use

---
+++ src/First.java
@@ -0,0 +1,15 @@
+public class First{
+    public static final String MY_NAME = "Patryk Bala";
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is: " + MY_NAME + ". " + "This is my first program");
+        System.out.println("Data:");
+        int a = 10;
+        int b = 25;
+        double c = 2.99;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("Result:");
+        System.out.println(b / a);
+    }
+}
