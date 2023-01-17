22cd427ebcd9aaad9301e34df1e22f78fe04ea63
Patryk Bala <patrykbala89@gmail.com>
2023-01-17 21:26:08 +0100

    first switch case program

--- src/ControlFunctions.java
+++ src/ControlFunctions.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 public class ControlFunctions {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        System.out.println("What is your cash target ?");
-        int goal= in.nextInt();
-        System.out.println("How many cash put you at year?");
-        int payment = in.nextInt();
-
-        int current = 0;
-        int years = 0;
-
-        while (current < goal) {
-            current = current + payment;
-            years++;
+        System.out.println("Choice is your: 1, 2, 3, 4, 5");
+        int choice = in.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("You choice is 1.");
+                break;
+            case 2:
+                System.out.println("You choice is 2.");
+                break;
+            case 3:
+                System.out.println("You choice is 3.");
+                break;
+            case 4:
+                System.out.println("You choice is 4.");
+                break;
+            case 5:
+                System.out.println("You choice is 5.");
+                break;
         }
-        System.out.println("You need " + years + " years to to get your " + goal + " cash goal.");
+        System.out.println("End of program");
     }
 }
