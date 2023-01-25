4e74c7d89e52a528610351c31b284953ea7b1574
Patryk Bala <patrykbala89@gmail.com>
2023-01-25 21:18:47 +0100

    first OOP program

--- src/CalendarTest.java
+++ src/CalendarTest.java
@@ -1,3 +1,4 @@
+import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -15,6 +16,39 @@ public class CalendarTest {
         int firstDayOfWeek = d.getFirstDayOfWeek();
 
         int indent = 0;
-        while ()
+        while (weekday != firstDayOfWeek){
+            indent++;
+            d.add(Calendar.DAY_OF_MONTH, -1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+        }
+
+        String[] weekdayNames = new DateFormatSymbols().getShortWeekdays();
+        do {
+            System.out.printf("%4s", weekdayNames[weekday]);
+            d.add(Calendar.DAY_OF_WEEK, 1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+        }
+        while (weekday != firstDayOfWeek);
+        System.out.println();
+
+        for (int i=1; i<=indent; i++);
+        System.out.print("     ");
+
+        d.set(Calendar.DAY_OF_MONTH, 1);
+        do {
+            int day = d.get(Calendar.DAY_OF_MONTH);
+            System.out.printf("%3d", day);
+
+            if (day == today) System.out.print("*");
+            else System.out.print(" ");
+
+            d.add(Calendar.DAY_OF_MONTH, 1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+
+            if (weekday == firstDayOfWeek) System.out.println();
+        }
+        while (d.get(Calendar.MONTH) == month);
+
+        if (weekday != firstDayOfWeek) System.out.println();
     }
 }
